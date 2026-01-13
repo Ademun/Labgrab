e690eccff33bf96a570ec544a6b69476afdcf8d9
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-13 09:07:08 +0300

    Service

--- internal/lab/service.go
+++ internal/lab/service.go
@@ -4,4 +4,12 @@ import "labgrab/internal/shared/api/dikidi"
 
 type Service struct {
 	dikidiClient *dikidi.Client
+	slotParser   *Parser
+}
+
+func NewService(client *dikidi.Client, slotParser *Parser) *Service {
+	return &Service{
+		dikidiClient: client,
+		slotParser:   slotParser,
+	}
 }
