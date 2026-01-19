8b748e35477e4820538a59699f863d6d8bff2884
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-19 22:41:11 +0300

    Config parsing and infra

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,31 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 type Config struct {
+	InfraConfig               InfraConfig
 	APIClientConfig           APIClientConfig           `yaml:"api_client"`
 	SubscriptionServiceConfig SubscriptionServiceConfig `yaml:"subscription_service"`
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config file, %v", err)
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal config, %v", err)
+	}
+
+	return &config, nil
 }
---
+++ pkg/config/infra.go
@@ -0,0 +1,16 @@
+package config
+
+type InfraConfig struct {
+	RedisConfig    RedisConfig
+	PostgresConfig PostgresConfig
+}
+
+type RedisConfig struct {
+	Address  string `env:"REDIS_ADDR"`
+	Password string `env:"REDIS_PASS"`
+	DB       int    `env:"REDIS_DB"`
+}
+
+type PostgresConfig struct {
+	ConnectionString string `env:"POSTGRES_CONN_STRING"`
+}
