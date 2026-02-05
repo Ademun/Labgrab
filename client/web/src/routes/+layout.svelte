651761a1632e71facae252ea70030f3a356e459f
Ademun <78663343+Ademun@users.noreply.github.com>
2026-02-05 11:22:31 +0300

    dev: additional dependencies

--- client/web/src/routes/+layout.svelte
+++ client/web/src/routes/+layout.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
-  import '../app.css';
-  import favicon from '$lib/assets/favicon.svg';
-  import {ModeWatcher} from "mode-watcher";
-  import {Navigation} from "$lib/components/navigation";
+    import '../app.css';
+    import favicon from '$lib/assets/favicon.svg';
+    import {ModeWatcher} from "mode-watcher";
+    import {Navigation} from "$lib/components/navigation";
+    import {configStore} from "$lib/stores/config.ts";
+    import {onMount} from "svelte";
+    import {userStore} from "$lib/stores/client.ts";
+    import {page} from '$app/state';
 
-  let {children} = $props();
+    let {children} = $props();
+
+    onMount(async () => {
+        try {
+            await configStore.load()
+        } catch (error) {
+            console.log('Failed to load app config', error)
+        }
+    })
+
+    $effect(() => {
+        if (page.data.user) {
+            userStore.setUser(page.data.user);
+        } else {
+            userStore.reset()
+        }
+    })
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon}/>
+    <link rel="icon" href={favicon}/>
 </svelte:head>
-<ModeWatcher />
+<ModeWatcher/>
 <div class="h-screen pb-24">
     {@render children?.()}
 </div>
