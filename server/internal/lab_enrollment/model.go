f56a4ef40195e3109ccde96eac67995c26c2a7dd
Ademun <78663343+Ademun@users.noreply.github.com>
2026-02-11 19:06:16 +0300

    dev: enrollment db models

---
+++ server/internal/lab_enrollment/model.go
@@ -0,0 +1,47 @@
+package lab_enrollment
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type JobStatus string
+
+const (
+	JobStatusQueued     JobStatus = "Queued"
+	JobStatusProcessing JobStatus = "Processing"
+	JobStatusCompleted  JobStatus = "Completed"
+)
+
+type DBEnrollmentJob struct {
+	UUID             uuid.UUID              `db:"uuid"`
+	UserUUID         uuid.UUID              `db:"user_uuid"`
+	SubscriptionUUID uuid.UUID              `db:"subscription_uuid"`
+	Status           JobStatus              `db:"job_status"`
+	AvailableDates   map[time.Time][]string `db:"available_dates"`
+	StartedAt        time.Time              `db:"started_at"`
+	CompletedAt      time.Time              `db:"completed_at"`
+}
+
+type JobResult string
+
+const (
+	JobResultSuccess JobResult = "Success"
+	JobResultFailed  JobResult = "Failed"
+)
+
+type DBJobResult struct {
+	JobUUID        uuid.UUID `db:"job_uuid"`
+	Result         JobResult `db:"result"`
+	ErrorMessage   string    `db:"error_message"`
+	EnrollmentUUID uuid.UUID `db:"enrollment_uuid"`
+}
+
+type DBEnrollment struct {
+	UUID               uuid.UUID `db:"uuid"`
+	UserUUID           uuid.UUID `db:"user_uuid"`
+	DikidiEnrollmentID int       `db:"dikidi_enrollment_id"`
+	VisitTime          time.Time `db:"visit_time"`
+	EnrolledAt         time.Time `db:"enrolled_at"`
+}
