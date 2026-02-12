f5722061399692f5c70dba64dc36e8b75e9e6991
Ademun <78663343+Ademun@users.noreply.github.com>
2026-02-12 17:08:55 +0300

    dev: user schema

---
+++ client/web/src/lib/api/schema/user.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const userResponseSchema = z.object({
+	username: z.string(),
+	name: z.optional(z.string()),
+	surname: z.optional(z.string()),
+	patronymic: z.optional(z.string()),
+	group_code: z.optional(z.string()),
+	phone_number: z.optional(z.number()),
+	photo_url: z.optional(z.string())
+});
+
+export type UserResponse = z.infer<typeof userResponseSchema>;
+
+export const userUpdateRequestSchema = z.object({
+	name: z.optional(z.string()),
+	surname: z.optional(z.string()),
+	patronymic: z.optional(z.string()),
+	group_code: z.optional(z.string()),
+	phone_number: z.optional(z.string()),
+	dikidi_password: z.optional(z.string())
+});
+
+export type UserUpdateRequest = z.infer<typeof userUpdateRequestSchema>;
