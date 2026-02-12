af35b24c3dcd85d2e103ba8262c82dbd189eda6f
Ademun <78663343+Ademun@users.noreply.github.com>
2026-02-12 17:24:36 +0300

    dev: subscriptions schema

---
+++ client/web/src/lib/api/schema/subscription.ts
@@ -0,0 +1,48 @@
+import { z } from 'zod';
+import type { userResponseSchema } from '$lib/api/schema/user.ts';
+
+export const labTypeSchema = z.object({
+	id: z.string(),
+	name_ru: z.string(),
+	name_en: z.string(),
+	needs_auditorium: z.boolean()
+});
+
+export type LabType = z.infer<typeof userResponseSchema>;
+
+export const labTopicSchema = z.object({
+	id: z.string(),
+	name_ru: z.string(),
+	name_en: z.string()
+});
+
+export type LabTopic = z.infer<typeof labTopicSchema>;
+
+export const StatusEnum = z.enum(['Active', 'Paused', 'Closed']);
+
+export const subscriptionResponseSchema = z.object({
+	uuid: z.string(),
+	lab_type: z.string(),
+	lab_topic: z.string(),
+	lab_number: z.number(),
+	lab_auditorium: z.optional(z.number()),
+	status: StatusEnum,
+	auto_enroll: z.boolean(),
+	any_date: z.boolean(),
+	created_at: z.date(),
+	closed_at: z.optional(z.date())
+});
+
+export type SubscriptionResponse = z.infer<typeof subscriptionResponseSchema>;
+
+export const createSubscriptionRequestSchema = z.object({
+	lab_type: z.string(),
+	lab_topic: z.string(),
+	lab_number: z.number(),
+	lab_auditorium: z.optional(z.number()),
+	auto_enroll: z.boolean(),
+	any_date: z.boolean(),
+	created_at: z.date()
+});
+
+export type CreateSubscriptionRequest = z.infer<typeof createSubscriptionRequestSchema>;
