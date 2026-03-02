657f8725c7ba0e673523b81acfb2f0f8cf7a5faa
Ademun <78663343+Ademun@users.noreply.github.com>
2026-03-02 18:44:39 +0300

    dev: refactored scraper a bit

--- server/internal/shared/api/dikidi/scraper.go
+++ server/internal/shared/api/dikidi/scraper.go
@@ -5,14 +5,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/imroc/req/v3"
 )
 
-func (c *Client) ScrapeSlotSourcesIDs(ctx context.Context, sourcesIDsProviderURL string) ([]int, error) {
-	doc, err := c.ScrapeDocument(ctx, sourcesIDsProviderURL)
+func (c *Client) ScrapeServices(ctx context.Context, client *req.Client, serviceProviderURL string) ([]int, error) {
+	doc, err := c.GetDocument(ctx, client, serviceProviderURL)
 	if err != nil {
-		return nil, fmt.Errorf("eror scraping document from url %s: %w", sourcesIDsProviderURL, err)
+		return nil, fmt.Errorf("eror scraping document from url %s: %w", serviceProviderURL, err)
 	}
 
 	idList := make([]int, 0)
@@ -22,7 +24,7 @@ func (c *Client) ScrapeSlotSourcesIDs(ctx context.Context, sourcesIDsProviderURL
 		if !exists {
 			return
 		}
-		var pageOptions HTMLPageOptions
+		var pageOptions APIHTMLPageOptions
 		err := json.Unmarshal([]byte(dataOptions), &pageOptions)
 		if err != nil {
 			parsingErr = errors.Join(parsingErr, err)
@@ -42,14 +44,21 @@ func (c *Client) ScrapeSlotSourcesIDs(ctx context.Context, sourcesIDsProviderURL
 	return idList, nil
 }
 
-func (c *Client) ScrapeDocument(ctx context.Context, sourcesIDsProviderURL string) (*goquery.Document, error) {
-	res, err := c.httpClient.Get(ctx, sourcesIDsProviderURL)
+func (c *Client) GetDocument(ctx context.Context, client *req.Client, url string) (*goquery.Document, error) {
+	resp, err := client.R().
+		SetContext(ctx).
+		SetHeaders(map[string]string{
+			"Sec-Fetch-Dest": "document",
+			"Sec-Fetch-Mode": "navigate",
+			"Sec-Fetch-Site": "none",
+			"Sec-Fetch-User": "?1",
+		}).
+		Get(url)
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(resp.String()))
 	if err != nil {
 		return nil, err
 	}
