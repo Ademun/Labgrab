3131ab211c8d79de864adfe16b22d566a2941001
Ademun <78663343+Ademun@users.noreply.github.com>
2026-03-02 18:29:22 +0300

    dev: better domain types

--- server/internal/shared/api/dikidi/model.go
+++ server/internal/shared/api/dikidi/model.go
@@ -1,6 +1,9 @@
 package dikidi
 
-import "labgrab/internal/shared/domain"
+import (
+	"labgrab/internal/shared/domain"
+	"time"
+)
 
 type Service struct {
 	ID int
@@ -17,4 +20,93 @@ type Event struct {
 	Spot       *int
 	Schedule   domain.Schedule
 	Link       string
+}
+
+type Booking struct {
+	ID         string
+	Name       string
+	Type       domain.LabType
+	Topic      domain.LabTopic
+	Number     int
+	Auditorium int
+	Spot       *int
+	Start      time.Time
+	End        time.Time
+}
+
+type CSRFTokenRequest struct {
+	PhoneNumber       string
+	TelegramCSRFToken string
+}
+
+type AuthRequest struct {
+	PhoneNumber       string
+	Password          string
+	CSRFToken         string
+	TelegramCSRFToken string
+}
+
+type ClientCookies struct {
+	CookieName *string
+	Token      *string
+	All        string
+}
+
+type EventReservationRequest struct {
+	EventID    int
+	ServicesID int
+	Time       string
+	Session    string
+}
+
+type EventReservationResponse struct {
+	BookingID      int
+	MasterID       string
+	DurationString string
+}
+
+type EnrollmentCheckRequest struct {
+	MasterID   int
+	ServicesID int
+	Time       string // YYYYMMDDHHmm
+	RecordID   int
+	Session    string
+	Phone      string
+	FirstName  string
+	LastName   string
+	Comments   string
+}
+
+type ReservationInfoRequest struct {
+	RecordID   int
+	MasterID   int
+	ServicesID int
+	Time       string // YYYYMMDDHHmm
+	Session    string
+}
+
+type CreateBookingRequest struct {
+	EventID   int
+	ServiceID int
+	Time      string // YYYYMMDDHHmm
+	RecordID  int
+	Session   string
+	Phone     string
+	FirstName string
+	LastName  string
+	Comments  string
+}
+
+type GetBookingsRequest struct {
+	Session string
+}
+
+type GetBookingsResult struct {
+	New []Booking
+	Old []Booking
+}
+
+type RemoveBookingRequest struct {
+	BookingID string
+	Session   string
 }
