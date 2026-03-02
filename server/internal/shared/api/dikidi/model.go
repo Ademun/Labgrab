4e861f553f87cd12abb8be8501e350f3f19f5e07
Ademun <78663343+Ademun@users.noreply.github.com>
2026-03-02 18:20:51 +0300

    dev: simple domain types

--- server/internal/shared/api/dikidi/model.go
+++ server/internal/shared/api/dikidi/model.go
@@ -1,92 +1,20 @@
 package dikidi
 
-type SlotResult struct {
-	Data *APISlotData
-	Err  error
+import "labgrab/internal/shared/domain"
+
+type Service struct {
+	ID int
 }
 
-type CSRFTokenRequest struct {
-	PhoneNumber       string
-	TelegramCSRFToken string
-}
-
-type AuthRequest struct {
-	PhoneNumber       string
-	Password          string
-	CSRFToken         string
-	TelegramCSRFToken string
-}
-
-type ClientCookies struct {
-	CookieName *string
-	Token      *string
-	All        string
-}
-
-type SlotReservationRequest struct {
-	MasterID   int
-	ServicesID int
-	Time       string
-	Session    string
-}
-
-type SlotReservationResponse struct {
-	RecordID       int
-	MasterID       string
-	DurationString string
-}
-
-type EnrollmentCheckRequest struct {
-	MasterID   int
-	ServicesID int
-	Time       string // YYYYMMDDHHmm
-	RecordID   int
-	Session    string
-	Phone      string
-	FirstName  string
-	LastName   string
-	Comments   string
-}
-
-type ReservationInfoRequest struct {
-	RecordID   int
-	MasterID   int
-	ServicesID int
-	Time       string // YYYYMMDDHHmm
-	Session    string
-}
-
-type CreateRecordRequest struct {
-	MasterID   int
-	ServicesID int
-	Time       string // YYYYMMDDHHmm
-	RecordID   int
-	Session    string
-	Phone      string
-	FirstName  string
-	LastName   string
-	Comments   string
-}
-
-type GetRecordsRequest struct {
-	Session string
-}
-
-type Record struct {
-	ID          string
-	Time        string
-	TimeTo      string
-	Duration    string
-	ServiceName string
-	MasterName  string
-}
-
-type GetRecordsResult struct {
-	New []Record
-	Old []Record
-}
-
-type RemoveRecordRequest struct {
-	RecordID string
-	Session  string
+type Event struct {
+	ID         int
+	ServiceID  int
+	Name       string
+	Type       domain.LabType
+	Topic      domain.LabTopic
+	Number     int
+	Auditorium int
+	Spot       *int
+	Schedule   domain.Schedule
+	Link       string
 }
