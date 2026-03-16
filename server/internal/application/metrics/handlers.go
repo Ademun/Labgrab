58094fade9a0e23f7d2d4169afc9f78d1789069c
Ademun <78663343+Ademun@users.noreply.github.com>
2026-03-16 11:22:20 +0300

    fix: changed metrics handler to use default prometheus handler

--- server/internal/application/metrics/handlers.go
+++ server/internal/application/metrics/handlers.go
@@ -4,16 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func RegisterRoutes(r *mux.Router) {
-	reg := prometheus.NewRegistry()
-	reg.MustRegister(
-		collectors.NewGoCollector(),
-		collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}),
-	)
-	r.Handle("/api/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{})).Methods(http.MethodGet)
+	r.Handle("/api/metrics", promhttp.Handler()).Methods(http.MethodGet)
 }
